b337066bcdc3be0018a108cf97ef7caae26e4e17
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-08-21 12:06:25 +0200

    update calc

--- ase/openmm_nacl_dissociation/openmmcalculator.py
+++ ase/openmm_nacl_dissociation/openmmcalculator.py
@@ -50,15 +50,15 @@ class Calculator0(OpenMMCalculator):
     Same as system1 but all water parameters are scaled by 1.1
     """
     def __init__(self):
-        self.xml_file = "openmm_input2/system0.xml"
-        self.pdb_file = "openmm_input2/topology.pdb"
+        self.xml_file = "openmm_input/system0.xml"
+        self.pdb_file = "openmm_input/topology.pdb"
         super().__init__(get_simulation(self.xml_file, self.pdb_file))
 
 
 class Calculator1(OpenMMCalculator):
     def __init__(self):
-        self.xml_file = "openmm_input2/system1.xml"
-        self.pdb_file = "openmm_input2/topology.pdb"
+        self.xml_file = "openmm_input/system1.xml"
+        self.pdb_file = "openmm_input/topology.pdb"
         super().__init__(get_simulation(self.xml_file, self.pdb_file))
 
 class DPSwitching:
